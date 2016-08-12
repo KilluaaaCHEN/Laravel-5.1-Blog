f02aeb9a0abb02684d982cfd63380bb6f54a7bee
Larry <Larry_e@163.com>
2016-08-12 10:29:22 +0800

    optimize api doc

--- app/Http/Controllers/Tools/DocController.php
+++ app/Http/Controllers/Tools/DocController.php
@@ -22,11 +22,10 @@ class DocController extends Controller
         $method = $request->get('method');
         $req = $request->get('request');
         $res = $request->get('response');
-
         $doc = '';
         if ($request->getMethod() == 'POST') {
             $uri = ltrim($uri, '{{domain}}');
-            $req_list = explode('//', $req);
+            $req_list = explode(PHP_EOL, $req);
             $res_list = json_decode($res, true);
 
             $req_dic = [
@@ -40,23 +39,23 @@ class DocController extends Controller
             $doc = <<<STR
 ### 1.0 $title 
 | URI | $uri  |   |   |   | 
-| ------------ | ------------ | ------------ | ------------ |  ------------ |
-|  请求方式 | $method  |  |   |   |
-|  输入参数 |  名称 | 含义  | 示例  | 必填 |
+| :----: | ------------ | ------------ | ------------ |  :----: |
+|  **请求方式** | $method  |  |   |   |
+|  **输入参数** |  **名称** | **含义**  | **示例**  | **必填** |
 
 STR;
             foreach ($req_list as $item) {
-                $i = explode(':', $item);
+                $not_null = strstr($item, '//') ? '  n' : '`y`';
+                $i = explode(':', ltrim($item, '//'));
                 if (count($i) > 1) {
                     $text = key_exists($i[0], $req_dic) ? $req_dic[$i[0]] : '&nbsp;';
                     $val = trim($i[1]);
-                    $doc .= "|   | `{$i[0]}`  | $text | $val | n | \n";
+                    $doc .= "|   | `{$i[0]}`  | $text | $val | $not_null | \n";
                 }
             }
-            $doc .= '|  输出参数 |  名称 | 含义  | 示例  | 类型 | ';
+            $doc .= '|  **输出参数** |  **名称** | **含义**  | **示例**  | **类**| ';
             if ($res_list) {
                 foreach ($res_list as $key => $val) {
-                    $val = trim($val);
                     $type = gettype($val);
                     if ($type == 'boolean') {
                         $val = $val ? 'true' : 'false';
--- resources/views/tools/doc.blade.php
+++ resources/views/tools/doc.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>API文档生成工具</title>
+    <title>API文档生成工具-Larry666</title>
     <!-- 新 Bootstrap 核心 CSS 文件 -->
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
@@ -52,24 +52,16 @@
         <input type="button" class="btn btn-primary" value="Copy To Clipboard" id="copy"/>
         <br/>
         <br/>
-        <textarea id="doc" class="form-control" rows=5 onChange="clip.setText(this.value)"
-                  title="">{!! $doc !!}</textarea>
+        <textarea id="doc" class="form-control" rows=5 onChange="clip.setText(this.value)" title="">{!! $doc !!}</textarea>
         <script type="text/javascript" src="{{asset('/plugins/zeroClipboard/ZeroClipboard.js')}}"></script>
         <script language="JavaScript">
-            var clip = null;
-            init();
-            function init() {
-                clip = new ZeroClipboard.Client();
-                clip.setHandCursor(true);
-
-                clip.addEventListener('mouseOver', my_mouse_over);
-                ZeroClipboard.setMoviePath("{{asset('/plugins/zeroClipboard/ZeroClipboard.swf')}}");
-                clip.glue('copy');
-            }
-
-            function my_mouse_over(client) {
-                clip.setText(document.getElementById('doc').value);
-            }
+            var clip = new ZeroClipboard.Client();
+            clip.setText(document.getElementById('doc').value);
+            ZeroClipboard.setMoviePath("{{asset('/plugins/zeroClipboard/ZeroClipboard.swf')}}");
+            clip.glue('copy');
+            clip.addEventListener('mouseup', function (client) {
+                document.getElementById('copy').value = 'Copy Success';
+            });
         </script>
     @endif
 
