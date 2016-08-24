c60905cd197417a1066aa158e210e8485a6541e6
larrychen <Larry_e@163.com>
2016-08-24 11:02:48 +0800

    fixed

--- resources/views/tools/qr.blade.php
+++ resources/views/tools/qr.blade.php
@@ -5,12 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>二维码生成-larry666</title>
     <link href="{{asset('plugins/qr/styles.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="{{asset('plugins/common/jquery.min.js')}}"></script>
     <script src="{{asset('plugins/qr/jquery-qrcode-0.14.0.min.js')}}"></script>
     <script src="{{asset('plugins/qr/scripts.js')}}"></script>
-    <title>二维码生成-larry666</title>
-    <!-- 新 Bootstrap 核心 CSS 文件 -->
-    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
 <div id="container"></div>
