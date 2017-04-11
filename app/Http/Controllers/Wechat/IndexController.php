8896d0981a9b79b8b7ff26929e2684fce5d2462d
KilluaChen <larry_e@163.com>
2017-04-11 14:58:56 +0800

    fixes

--- app/Http/Controllers/Wechat/IndexController.php
+++ app/Http/Controllers/Wechat/IndexController.php
@@ -17,14 +17,31 @@ class IndexController extends Controller
      */
     public function callback()
     {
-        $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function ($message) {
-            return "欢迎关注:" . $message;
+//        $wechat = app('wechat');
+//        $wechat->server->setMessageHandler(function ($message) {
+//            return "欢迎关注:" . $message;
+//        });
+//
+//        Log::info('return response.');
+//
+//        return $wechat->server->serve();
+
+        $options = [
+            'debug' => true,
+            'app_id' => env('WECHAT_APPID'),
+            'secret' => env('WECHAT_SECRET'),
+            'token' => env('WECHAT_TOKEN'),
+            'log' => [
+                'level' => 'debug',
+                'file' => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+            ],
+        ];
+        $app = new Application($options);
+        $app->server->setMessageHandler(function ($message) {
+            return "您好！欢迎关注我!".$message;
         });
-
-        Log::info('return response.');
-
-        return $wechat->server->serve();
+        $response = $app->server->serve();
+        return $response;
     }
 
 }
