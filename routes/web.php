b22e3531b2c9a09660bc8a90e51ae75e30bb3baf
KilluaChen <larry_e@163.com>
2017-04-19 18:21:40 +0800

    hooks

--- routes/web.php
+++ routes/web.php
@@ -40,13 +40,12 @@ Route::any('/generate/code', ['as' => 'generate_code', 'uses' => 'Tools\CodeCont
 Route::any('hooks', function () {
     $secret = env('WEBHOOKS_SECRET');
     $path = env('WEBHOOKS_PATH');
-    $postdata = file_get_contents("php://input");
     $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
     if ($signature) {
-        $hash = "sha1=" . hash_hmac('sha1', $postdata, $secret);
+        $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
         if (strcmp($signature, $hash) == 0) {
             echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-            var_dump('Success');
+            exit();
         }
     }
     http_response_code(404);
