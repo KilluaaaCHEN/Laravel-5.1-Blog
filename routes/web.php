46420ea946cb0c95f6df14c3ed7bfd9735087632
KilluaChen <larry_e@163.com>
2017-04-19 18:10:22 +0800

    add hooks

--- routes/web.php
+++ routes/web.php
@@ -40,15 +40,13 @@ Route::any('/generate/code', ['as' => 'generate_code', 'uses' => 'Tools\CodeCont
 Route::any('hooks', function () {
     $secret = env('WEBHOOKS_SECRET');
     $path = env('WEBHOOKS_PATH');
-    $signature = $_SERVER['X-Hub-Signature'];
+    $postdata = file_get_contents("php://input");
+    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
     if ($signature) {
-        $postdata = file_get_contents("php://input");
         $hash = "sha1=" . hash_hmac('sha1', $postdata, $secret);
         if (strcmp($signature, $hash) == 0) {
             echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-            exit();
-        }else{
-            var_dump($secret,$path,$signature,$hash);
+            var_dump('Success');
         }
     }
     http_response_code(404);
