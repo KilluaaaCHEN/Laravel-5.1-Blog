34df83fad1a7417b0dca341ff23e9533f5547e8d
KilluaChen <larry_e@163.com>
2017-04-20 14:15:47 +0800

    roll-back

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
     </script>
 @stop
 @section('content')
-    <title>Larry的博客 - Test</title>
+    <title>Larry的博客</title>
     <div class="panel panel-default">
         <div class="panel-heading">
             <h5 class="panel-title">
