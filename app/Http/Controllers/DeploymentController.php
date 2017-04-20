194c5addcf80cbef6ae17559a65d1a2a6a79958e
KilluaChen <larry_e@163.com>
2017-04-20 11:18:37 +0800

    add hooks

---
+++ app/Http/Controllers/DeploymentController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+/**
+ * Class DeploymentController
+ *
+ * @package App\Http\Controllers
+ */
+class DeploymentController extends Controller
+{
+    /**
+     * @param Request $request
+     */
+    public function deploy(Request $request)
+    {
+        $commands = ['cd /html/blog', 'git pull'];
+        $signature = $request->header('X-Hub-Signature'); // $headers = getallheaders(); $headers['X-Hub-Signature']
+        $payload = file_get_contents('php://input');
+        if ($this->isFromGithub($payload, $signature)) {
+            foreach ($commands as $command) {
+                shell_exec($command);
+            }
+            http_response_code(200);
+        } else {
+            abort(403);
+        }
+    }
+
+    /**
+     * @param $payload
+     * @param $signature
+     * @return bool
+     */
+    private function isFromGithub($payload, $signature)
+    {
+        return 'sha1=' . hash_hmac('sha1', $payload, env('WEBHOOKS_SECRET'), false) === $signature;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -37,19 +37,4 @@ Route::get('/qr-code', ['as' => 'qr_code', 'uses' => 'Tools\QrCodeController@gen
 Route::any('/generate/doc', ['as' => 'generate_doc', 'uses' => 'Tools\DocController@generate']);
 Route::any('/generate/code', ['as' => 'generate_code', 'uses' => 'Tools\CodeController@generate']);
 
-Route::any('hooks', function () {
-    $secret = env('WEBHOOKS_SECRET');
-    $path = env('WEBHOOKS_PATH');
-    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-    if ($signature) {
-        $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
-        if (strcmp($signature, $hash) == 0) {
-            $cmd = "cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1";
-            $cmd = "sudo -u root -S $cmd < ~/.sudopass/sudopass.secret";
-            exec($cmd);
-            echo shell_exec($cmd);
-            exit();
-        }
-    }
-    http_response_code(404);
-});
+Route::post('/hooks','DeploymentController@deploy');
