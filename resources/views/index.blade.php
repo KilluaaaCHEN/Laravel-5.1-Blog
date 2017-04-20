4862bf499193200962497bac1a64b7de5d3bc879
KilluaChen <larry_e@163.com>
2017-04-20 11:42:35 +0800

    test hook

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
     </script>
 @stop
 @section('content')
-    <title>Larry的博客</title>
+    <title>Larry的博客 - test</title>
     <div class="panel panel-default">
         <div class="panel-heading">
             <h5 class="panel-title">
