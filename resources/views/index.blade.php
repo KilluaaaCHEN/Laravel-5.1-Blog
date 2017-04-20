aa44a4879d154e9c5012f4cb8a881b895b5065bc
KilluaChen <larry_e@163.com>
2017-04-20 16:11:43 +0800

    test2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
     </script>
 @stop
 @section('content')
-    <title>Larry的博客 -1</title>
+    <title>Larry的博客 -2</title>
     <div class="panel panel-default">
         <div class="panel-heading">
             <h5 class="panel-title">
