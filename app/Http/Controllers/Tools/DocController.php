686b14792a5bdd1eadaca8a7a68bcb76cf9bf722
KilluaChen <larry_e@163.com>
2017-07-21 14:00:58 +0800

    fixes

--- app/Http/Controllers/Tools/DocController.php
+++ app/Http/Controllers/Tools/DocController.php
@@ -178,6 +178,9 @@ STR;
         $this->filterAttr($res_list);
         //数组牌组,对象放最后
         $after = [];
+        if (empty($res_list)) {
+            return '';
+        }
         foreach ($res_list as $i => $item) {
             if (is_array($item) && !isset($item['sec']) && !isset($item['usec']) && $i != '_id') {
                 $after[$i] = $item;
