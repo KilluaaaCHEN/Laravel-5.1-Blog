eb3281fdcf7527e1a7f815740e1cff419d26af88
KilluaChen <larry_e@163.com>
2018-04-09 17:53:57 +0800

    analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,23 @@
     <script src="{{asset('plugins/common/jquery.min.js')}}"></script>
     <script src="{{asset('plugins/boot/bootstrap.js')}}"></script>
     <script src="{{asset('front/site.js')}}"></script>
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?2238665cc2b0cf54d82cffd514e1cb0d";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-99000479-1', 'auto');
+        ga('send', 'pageview');
+    </script>
     @yield('head')
 </head>
 <body>
@@ -70,20 +87,6 @@
     <p class="text-center">
         Copyright &copy; <?= date('Y') ?> Killua Blog<br/>
         渝ICP备15006619号-1<br/>
-        <script type="text/javascript">
-            var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-            document.write(unescape("%3Cspan id='cnzz_stat_icon_1255735945'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1255735945%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
-        </script>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-99000479-1', 'auto');
-            ga('send', 'pageview');
-
-        </script>
     </p>
 </footer>
 <a href="javascript:void(0)" class="go-top" title="Back to top">
